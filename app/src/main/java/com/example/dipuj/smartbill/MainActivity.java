3217c1dd994dba738edf9c33a25bdf5850aa2d22
rockjames007 <30474748+rockjames007@users.noreply.github.com>
2018-02-27 04:14:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dipuj/smartbill/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.dipuj.smartbill;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private Button btnSignup, btnLogin;
+    private TextView btnReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            //Get Firebase auth instance
+            auth = FirebaseAuth.getInstance();
+
+            if (auth.getCurrentUser() != null) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                finish();
+            }
+
+            // set the view now
+            setContentView(R.layout.activity_main);
+
+            inputEmail = (EditText) findViewById(R.id.emailid);
+            inputPassword = (EditText) findViewById(R.id.passwd);
+            btnLogin = (Button) findViewById(R.id.login);
+            btnReset = (TextView) findViewById(R.id.fgpasswd);
+
+            //Get Firebase auth instance
+            auth = FirebaseAuth.getInstance();
+           btnReset.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, ResetPasswordActivity.class));
+                }
+            });
+
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String email = inputEmail.getText().toString();
+                    final String password = inputPassword.getText().toString();
+
+                    if (TextUtils.isEmpty(email)) {
+                        Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (TextUtils.isEmpty(password)) {
+                        Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+
+                    //authenticate user
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                    // the auth state listener will be notified and logic to handle the
+                                    // signed in user can be handled in the listener.
+                                    if (!task.isSuccessful()) {
+                                        // there was an error
+                                        if (password.length() < 6) {
+                                            inputPassword.setError(getString(R.string.minimum_password));
+                                        } else {
+                                            Toast.makeText(MainActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                        }
+                                    } else {
+                                        Intent intent = new Intent(MainActivity.this, userActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                }
+            });
+        }
+    }
+
